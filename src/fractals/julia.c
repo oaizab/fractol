36f55f9c6e8b5f8ee01a33842e1f8ceddf1c18e1
oaizab <omaraizab98@gmail.com>
2022-01-14 20:17:55 +0100

    optimizing julia

--- src/fractals/julia.c
+++ src/fractals/julia.c
@@ -6,7 +6,7 @@
 /*   By: oaizab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 10:15:22 by oaizab            #+#    #+#             */
-/*   Updated: 2022/01/13 21:21:48 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/01/14 20:03:07 by oaizab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,12 @@ void	julia(t_env *env)
 	int			x;
 	int			y;
 	double		im[HEIGHT + 1];
-	t_complex	c;
-	t_complex	z;
+	t_complex	c[2];
 
 	im[HEIGHT] = -2.7 + (1.7 + 2.7) * env->mouse_x / WIDTH;
 	im[0] = -1.2 + (1.2 + 1.2) * env->mouse_y / HEIGHT;
-	c = ft_complex(im[HEIGHT], im[0]);
+	c[0].re = im[HEIGHT];
+	c[0].im = im[0];
 	y = -1;
 	while (++y < HEIGHT)
 		im[y] = env->min_im + (env->max_im - env->min_im) * y / HEIGHT;
@@ -60,8 +60,9 @@ void	julia(t_env *env)
 		y = -1;
 		while (++y < HEIGHT)
 		{
-			z = ft_complex(im[HEIGHT], im[y]);
-			ft_put_pixel(env, x, y, draw(env, z, c));
+			c[1].re = im[HEIGHT];
+			c[1].im = im[y];
+			ft_put_pixel(env, x, y, draw(env, c[1], c[0]));
 		}
 	}
 }
